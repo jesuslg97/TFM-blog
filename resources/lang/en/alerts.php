e4074c0cb00be405faf730b4cf05010931c8b42a
Jesus <jelopezgonzalez@gmail.com>
2023-04-05 10:13:14 +0100

    add translates

---
+++ resources/lang/en/alerts.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    //GENERAL
+    'successful_message' => 'Correcto: ',
+    'info_message' => 'Mensaje de información: ',
+    'warning_message' => 'Mensaje de advertencia: ',
+    'danger_message' => 'Incorrecto: ',
+    'error_message' => 'Error',
+
+    //CATEGORIES
+    'categories_created_successfully' => 'Categoría creada correctamente',
+    'categories_update_successfully' => 'Categoría actualizada correctamente',
+    'categories_delete_successfully' => 'Categoría borrada correctamente',
+    'categories_delete_error' => 'Categoría no borrada',
+
+    //POSTS
+    'posts_created_successfully' => 'Post creado correctamente',
+    'posts_update_successfully' => 'Post actualizado correctamente',
+    'posts_delete_successfully' => 'Post borrado correctamente',
+    'posts_delete_error' => 'Post no borrado',
+
+    //COMMENTS
+    'comments_created_successfully' => 'Comentario creado correctamente',
+    'comments_update_successfully' => 'Comentario actualizado correctamente',
+    'comments_delete_successfully' => 'Comentario borrado correctamente',
+    'comments_delete_error' => 'Comentario no borrado',
+
+    //LANGUAGES
+    'languages_created_successfully' => 'Idioma creado correctamente',
+    'languages_update_successfully' => 'Idioma actualizado correctamente',
+    'languages_delete_successfully' => 'Idioma borrado correctamente',
+    'languages_delete_error' => 'Idioma no borrado',
+
+    //CATEGORY_LANG
+    'categoryLang_created_successfully' => 'Idioma de la categoría creada correctamente',
+    'categoryLang_update_successfully' => 'Idioma de la categoría actualizada correctamente',
+    'categoryLang_delete_successfully' => 'Idioma de la categoría borrada correctamente',
+    'categoryLang_delete_error' => 'Idioma de la categoría no borrada',
+
+    //POST_LANG
+    'postLang_created_successfully' => 'Idioma del post creado correctamente',
+    'postLang_update_successfully' => 'Idioma del post actualizado correctamente',
+    'postLang_delete_successfully' => 'Idioma del post borrado correctamente',
+    'postLang_delete_error' => 'Idioma del post no borrado',
+
+];
---
+++ resources/lang/en/string.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    //HOME
+    'home_title' => 'TFM-Blog',
+
+    //BUTTONS
+    'create_bn' => 'Crear',
+    'edit_btn' => 'Editar',
+    'delete_btn' => 'Borrar',
+    'search_btn' => 'Buscar',
+
+    //TITLES
+    'list_title_category' =>'Lista de categorías',
+    'list_title_post' =>'Lista de posts',
+    'list_title_comment' =>'Lista de comentarios',
+    'list_title_language' =>'Lista de idiomas',
+    'list_title_categoryLang' =>'Lista de idiomas de categorías',
+    'list_title_postLang' =>'Lista de idiomas de posts',
+
+    //TABLES
+    'id_header' => 'id',
+    'name_header' => 'Nombre',
+    'actions_header' => 'Acciones',
+
+    //CATEGORIES
+    'category_image' => 'Link de la imagen',
+    'create_category' => 'Crear categoría',
+    'edit_category' => 'Editar categoría',
+
+    //POSTS
+    'search_post_placeholder' => 'Buscar post',
+    'create_post' => 'Crear post',
+    'edit_post' => 'Editar post',
+
+    //COMMENTS
+    'search_comment_placeholder' => 'Buscar comentario',
+    'create_comment' => 'Crear comentario',
+    'edit_comment' => 'Editar comentario',
+    'comment' => 'Comentario',
+
+    //LANGUAGES
+    'search_language_name_placeholder' => 'Buscar nombre del idioma',
+    'search_language_iso_placeholder' => 'Buscar ISO del idioma',
+    'search_language_placeholder' => 'Buscar idioma',
+    'create_language' => 'Crear idioma',
+    'edit_language' => 'Editar idioma',
+    'language_name' => 'Nombre',
+    'language_ISO' => 'Código ISO',
+
+    //CATEGORY_LANG
+    'search_categoryLang_name_placeholder' => 'Buscar nombre del idioma de la categoría',
+    'create_categoryLang' => 'Crear idioma de la categoría',
+    'edit_categoryLang' => 'Editar idioma de la categoría',
+    'categoryLang_name' => 'Nombre',
+    'categoryLang_description' => 'Descripción',
+
+    //POST_LANG
+    'search_postLang_title_placeholder' => 'Buscar título del idioma del post',
+    'search_postLang_placeholder' => 'Buscar idiomas de posts',
+    'create_postLang' => 'Crear idiomas de posts',
+    'edit_postLang' => 'Editar idiomas de posts',
+    'postLang_title' => 'Título',
+    'postLang_description' => 'Descripción',
+    'postLang_information' => 'Información',
+
+];
